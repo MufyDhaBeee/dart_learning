267f045d9d1885e9e711e47de150257012120886
MufyDhaBeee <mufeedabeegum93@gmail.com>
2026-03-27 11:03:38 +0530

    dsafdsaf

---
+++ first_learning/bin/first_learning.dart
@@ -0,0 +1,6 @@
+import 'package:first_learning/first_learning.dart' as first_learning;
+
+void main(List<String> arguments) {
+  print("object");
+  print('Hello world: ${first_learning.calculate()}!');
+}
---
+++ first_learning/test/first_learning_test.dart
@@ -0,0 +1,8 @@
+import 'package:first_learning/first_learning.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
