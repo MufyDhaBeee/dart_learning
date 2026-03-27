4489f3b5e92ea849ac58b8caf05f525845561697
MufyDhaBeee <mufeedabeegum93@gmail.com>
2026-03-27 15:40:25 +0530

    ntg

--- dart_intro.dart
+++ dart_intro.dart
@@ -1,5 +1,23 @@
 void main(){
  print('Hello world!');
- var name = "john";
+ //---------------------------printing name-------------------------------------
+ var name = "Pomelo";
  print(name);
+//----------------------------variable name--------------------------------------
+var firstName = "John";
+var lastName = "Doe";
+print("Full name is $firstName $lastName");
+// --------------------------for basic calculation----------------------------------
+int num1 = 60;
+int num2 = 30;
+//calculate
+int sum = num1+num2;
+int sub = num1-num2;
+int multi = num1*num2;
+double div = num1/num2;
+//display output
+print("The sum is $sum");
+print("The sub is $sub");
+print("The multi is $multi");
+print('The div is $div');
 }
