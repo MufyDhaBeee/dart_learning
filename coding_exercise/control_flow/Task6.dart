46126229fc4f30048a4ebc034b88e3814db28d7f
MufyDhaBeee <mufeedabeegum93@gmail.com>
2026-04-28 12:55:12 +0530

    control flow

---
+++ coding_exercise/control_flow/Task6.dart
@@ -0,0 +1,10 @@
+//Task 6 : Check if the number 7 is even or odd and print the result.
+// Hint: Use the  %  (modulus) operator. If number % 2 == 0, it is even.
+void main(){
+  int number = 7;
+  if (number % 2 == 0){
+    print("It is even");
+  }else{
+    print("It is odd");
+  }
+}
---
+++ coding_exercise/variables_and_datatypes/Task5.dart
@@ -0,0 +1,6 @@
+////Task 5:  Declare String city = 'Kozhikode' and int year = 2024. Print: I live in [city] since [year].
+void main(){
+    String city = "Kozhikode";
+    int year = 2024;
+    print("I live in $city since $year");
+}
