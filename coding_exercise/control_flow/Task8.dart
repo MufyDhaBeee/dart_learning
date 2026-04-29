5f20cda175ffe6009afbe9fd1d2b11f350e019a0
MufyDhaBeee <mufeedabeegum93@gmail.com>
2026-04-29 14:52:54 +0530

    task 8 completed

---
+++ coding_exercise/control_flow/Task8.dart
@@ -0,0 +1,7 @@
+//k: Use a for loop to print numbers 1 through 5, each on a new line.
+// Hint: for (int i = 1; i <= 5; i++) { ... }  — i starts at 1, stops when > 5
+void main(){
+for(int i = 1;  i<= 5; i++){
+  print(i);
+}
+}
