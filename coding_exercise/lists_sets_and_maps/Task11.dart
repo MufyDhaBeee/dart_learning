bfcad516005ab21ab46fece829ac09870797d079
MufyDhaBeee <mufeedabeegum93@gmail.com>
2026-05-02 19:03:07 +0530

    task 9 to 11 is completed

---
+++ coding_exercise/lists_sets_and_maps/Task11.dart
@@ -0,0 +1,8 @@
+//Task: Create a List of 3 fruits. Print the whole list, then print only the second fruit.
+// Hint: List<String> fruits = ['Apple','Banana','Mango'];  — Index starts at 0, so second item is index 1.
+
+void main(){
+  List<String>fruits = ['Apple', 'Banana', 'Mango'];
+  print('whole list: $fruits');
+  print('second fruit: ${fruits[1]}');
+}
