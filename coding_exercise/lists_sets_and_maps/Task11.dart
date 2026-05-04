a31dffa4d75d22d717b611fc24ba2d4e3c33942a
MufyDhaBeee <mufeedabeegum93@gmail.com>
2026-05-04 12:31:01 +0530

    task 12 completed

--- coding_exercise/lists_sets_and_maps/Task11.dart
+++ coding_exercise/lists_sets_and_maps/Task11.dart
@@ -1,5 +1,4 @@
 //Task: Create a List of 3 fruits. Print the whole list, then print only the second fruit.
-// Hint: List<String> fruits = ['Apple','Banana','Mango'];  — Index starts at 0, so second item is index 1.
 
 void main(){
   List<String>fruits = ['Apple', 'Banana', 'Mango'];
---
+++ coding_exercise/lists_sets_and_maps/Task12.dart
@@ -0,0 +1,11 @@
+//Task: Start with a list ['Red', 'Blue']. Add 'Green', then remove the first color. Print the
+// final list.
+
+void main(){
+  List<String> Colors = ['Red', 'Blue'];
+  print("Colors: $Colors");
+  Colors.add('Green');
+  print("Color added: $Colors");  // Add Green
+  Colors.removeAt(0);
+  print("Remove Color $Colors"); //remove index 0
+}
